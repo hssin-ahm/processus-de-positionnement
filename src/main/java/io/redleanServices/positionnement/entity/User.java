b295bd254750dfb7539993cb90c496ce61170572
Ahmed Hassine <ahm.hssin@gmail.com>
2021-08-19 22:20:20 +0200

    change in user class

--- src/main/java/io/redleanServices/positionnement/entity/User.java
+++ src/main/java/io/redleanServices/positionnement/entity/User.java
@@ -9,6 +9,7 @@ public class User {
     private String userName;
     private String userFirstName;
     private String userLastName;
+    private String email;
     private String userPassword;
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "USER_ROLE",
@@ -60,4 +61,13 @@ public class User {
     public void setRole(Set<Role> role) {
         this.role = role;
     }
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
 }
